f949ef9769d173ee2f964be724a73372bf5a29e3
sahlminkok <sahlminkok7@gmail.com>
2023-12-17 15:15:29 +0100

    Build the layout of the app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
 function App() {
   return (
-    <div className="App">
-      <h1>Hello, World!</h1>
-    </div>
+    <>
+      <Logo />
+      <Form />
+      <PackagingList />
+      <Stats />
+    </>
   );
 }
+
+const Logo = () => (
+  <div className="logo">
+    <h1>🌴 Far Away 💼</h1>
+  </div>
+);
+
+const Form = () => (
+  <div className="add-form">
+    <h3>What do you need for your 😍 trip?</h3>
+  </div>
+);
+
+const PackagingList = () => (
+  <div className="list">
+    LIST
+  </div>
+);
+
+const Stats = () => (
+  <footer className="stats">
+    <em>You have X items on your list, and you already packed X (X%)</em>
+  </footer>
+);
 
 export default App;
